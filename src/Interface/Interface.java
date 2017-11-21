7522cb5c1e1952b1f1caf67cd3fdb1e703df3fd2
ludovicm67 <ludovicmuller1@gmail.com>
2017-11-21 10:31:33 +0100

    fix #7: rendre les éléments visibles seulement à la fin

--- src/Interface/Interface.java
+++ src/Interface/Interface.java
@@ -12,22 +12,24 @@ public class Interface extends JFrame {
   static String textButton = "Sélectionner un fichier SVG";
 
   public Interface() {
-    //mise en place de la fenêtre
+    // mise en place de la fenêtre
     setTitle(title);
     setSize(width, height);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setVisible(true);
 
-    //mise en place du contenant
+    // mise en place du contenant
     JPanel content = new JPanel();
     setContentPane(content);
     content.setBackground(Color.orange);
 
-    //mise en place du contenu
+    // mise en place du contenu
     JLabel label = new JLabel("Hello world!");
     getContentPane().add(label);
 
     JButton showChooseFile = new ChooseFileButton(textButton);
     getContentPane().add(showChooseFile);
+
+    // on rend le tout visible
+    setVisible(true);
   }
 }
